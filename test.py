c93e56e9f662e47e85551857a66685d351509567
Roland Krasser <roland.krasser@gmail.com>
2025-07-21 16:39:23 +0200

    add obs, handling null values

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import polars as pl
 data = {
     "id": [1, 2, 3, 4, 5, 6, 7, 8],
     "success": [1, 0, 1, 0, 0, 1, 0, 1],
+    "name": ["bob", "tom", "mick", None, "ben", "eva", "udo", "pam"],
     "boost": [1, 0, 1, 1, 0, 1, 0, None]
 }
 
@@ -12,23 +13,33 @@ df.filter(pl.col('success') == 1)
 def describe(df: pl.DataFrame) -> pl.DataFrame:
 
     description_pl = df.describe()
+    df_rows = df.shape[0]
+    df_cols = df.shape[1]
 
     out_variable = description_pl.drop(pl.col('statistic')).columns
     out_type = description_pl.drop(pl.col('statistic')).dtypes
     out_type = [str(x) for x in out_type]
+    out_obs = [df_rows] * df_cols
     out_na = description_pl.filter(pl.col('statistic') == 'null_count').drop(pl.col('statistic')) 
+    out_na = out_na.cast(pl.Int64)
     out_na = out_na[0, ].transpose().to_series().cast(pl.Int64).to_list()
+    out_na_pct = [x * 100.00 / df_rows for x in out_na]
     out_min = description_pl.filter(pl.col('statistic') == 'min').drop(pl.col('statistic'))
+    out_min = out_min.cast(pl.Int64, strict = False)
     out_min = out_min[0, ].transpose().to_series().to_list()
     out_mean = description_pl.filter(pl.col('statistic') == 'mean').drop(pl.col('statistic'))
+    out_mean = out_mean.cast(pl.Int64, strict = False)
     out_mean = out_mean[0, ].transpose().to_series().to_list()
     out_max = description_pl.filter(pl.col('statistic') == 'max').drop(pl.col('statistic'))
+    out_max = out_max.cast(pl.Int64, strict = False)
     out_max = out_max[0, ].transpose().to_series().to_list()
 
     out = pl.DataFrame({
         "variable": out_variable,
         "type": out_type,
+        "obs": out_obs,
         "na": out_na,
+        "na_pct": out_na_pct,
         "min": out_min,
         "mean": out_mean,
         "max": out_max
